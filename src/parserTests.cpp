04c9014f4d70aad70427418c9e8a3c711a68b39f
pavel <plavik2@gmail.com>
2025-07-25 14:48:51 +0200

    fix conflict in parser test

--- src/parserTests.cpp
+++ src/parserTests.cpp
@@ -1,65 +1,12 @@
-<<<<<<< HEAD
-// #include "../Include/Command.hpp"
-// #include <iostream>
-// #include <vector>
-
-// // Helper function to print a parsed message for verification.
-// void print_message(const std::string &raw_line, const Command &cmd) {
-//   std::cout << "--> " << raw_line << std::endl;
-//   if (cmd.getPrefix().has_value())
-//     std::cout << "  Prefix: " << cmd.getPrefix().value() << std::endl;
-//   else
-//     std::cout << "  Prefix: (none)" << std::endl;
-//   std::cout << "  Command: " << cmd.getCommand() << std::endl;
-//   if (!cmd.getParams().empty()) {
-//     std::cout << "  Params (" << cmd.getParams().size() << "):" << std::endl;
-//     for (size_t i = 0; i < cmd.getParams().size(); ++i) {
-//       std::cout << "    " << cmd.getParams()[i].name << ": "
-//                 << cmd.getParams()[i].value << std::endl;
-//     }
-//   }
-//   std::cout << "----------------------------------------" << std::endl;
-// }
-// int main() {
-//   std::vector<std::string> test_cases = {
-//       // Common cases
-//       ":Nick!user@host JOIN #channel",
-//       ":irc.server.net 001 MyNick :Welcome to the Internet Relay Network "
-//       "MyNick",
-//       "PRIVMSG #chat :Hello everyone! This is a test.", "PING :123456789",
-
-//       // Cases with multiple middle params
-//       "MODE #channel +o user1", ":server.name MODE #channel +v user2",
-
-//       // Edge cases
-//       "QUIT",          // Command with no params
-//       "NICK new_nick", // Command with one param
-//       ":server.name NOTICE Auth :*** Looking up your hostname...", // Server
-//                                                                    // notice
-//       ":some_user!~user@host PRIVMSG #another_channel :This is a message with "
-//       "a : colon inside."};
-
-//   for (const auto &line : test_cases) {
-//     try {
-//       Command msg = Command::parse(line);
-//       print_message(line, msg);
-//     } catch (const std::runtime_error &e) {
-//       std::cerr << "Failed to parse '" << line << "': " << e.what()
-//                 << std::endl;
-//       std::cerr << "----------------------------------------" << std::endl;
-//     }
-//   }
-
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   parserTests.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: auplisas <auplisas@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: psenko <psenko@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 01:36:24 by mratke            #+#    #+#             */
-/*   Updated: 2025/07/25 04:39:03 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/07/25 14:48:15 by psenko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,6 +62,5 @@
 //     }
 //   }
 
->>>>>>> origin/main
 //   return 0;
 // }
